9c24cb47dc0dc3a6c09e99d174429aed8b74f565
Galen Molk <76459193+galenmolk@users.noreply.github.com>
2023-02-18 09:49:24 -0500

    Form validation

--- src/components/shared/InputField.jsx
+++ src/components/shared/InputField.jsx
@@ -1,9 +1,18 @@
-function InputField({ label, value }) {
+function InputField({ label, value, handleChange, placeholder, type, min, max, required }) {
     return (
         <div>
             <label htmlFor="inputField">{label}</label>
             <br/>
-            <input id="inputField" defaultValue={value}/>
+            <input 
+                id="inputField" 
+                defaultValue={value}
+                onChange={handleChange}
+                placeholder={placeholder}
+                type={type}
+                min={min}
+                max={max}
+                required={required}
+            />
             <br/>
         </div>
     )
--- src/pages/CharacterCreatorPage.jsx
+++ src/pages/CharacterCreatorPage.jsx
@@ -1,19 +1,107 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import TavernContext from '../context/TavernContext'
 import InputField from '../components/shared/InputField'
 
 function CharacterCreatorPage() {
 
+    window.onbeforeunload = function (event) {
+        event = event || window.event
+
+        const contextName = (name === '' ? 'This character ' : name)
+
+        const message = contextName + ' will not be saved. Exit page?'
+
+        if (event) {
+            event.returnValue = message
+        }
+
+        return message
+    }
+
+    const [name, setName] = useState()
+    const [health, setHealth] = useState()
+    const [defense, setDefense] = useState()
+    const [speed, setSpeed] = useState()
+
     const { activeCharacter } = useContext(TavernContext)
+
+    useEffect(() => {
+        if (activeCharacter === null) {
+            return
+        }
+
+        setName(activeCharacter.name)
+        setHealth(activeCharacter.health)
+        setDefense(activeCharacter.defense)
+        setSpeed(activeCharacter.speed)
+        
+    }, [activeCharacter])
+
+    const handleSubmit = (event) => {
+        console.log('form submitted')
+    }
+
+    const handleNameChange = (event) => {
+        console.log('new name: ' + event.target.value)
+        setName(event.target.value)
+    }
+
+    const handleHealthChange = (event) => {
+        console.log('new health: ' + event.target.value)
+        setHealth(event.target.value)
+    }
+
+    const handleDefenseChange = (event) => {
+        console.log('new defense: ' + event.target.value)
+        setDefense(event.target.value)
+    }
+
+    const handleSpeedChange = (event) => {
+        console.log('new speed: ' + event.target.value)
+        setSpeed(event.target.value)
+    }
 
     return (
         <>
-            <InputField label='Character Name:' value={activeCharacter.name}/>
-            <InputField label='Health:' value={activeCharacter.health}/>
-            <InputField label='Defense:' value={activeCharacter.defense}/>
-            <InputField label='Speed:' value={activeCharacter.speed}/>
+            <form onSubmit={handleSubmit}>
+                <InputField 
+                    label={'Name:'}
+                    value={name} 
+                    handleChange={handleNameChange}
+                    placeholder='Name'
+                    type='text'
+                    required={true}
+                />
+                <InputField 
+                    label={'Health:'}
+                    value={health} 
+                    handleChange={handleHealthChange} 
+                    placeholder='Health'
+                    type='number'
+                    min="0"
+                    required={true}
+                />
+                <InputField 
+                    label={'Defense:'}
+                    value={defense} 
+                    handleChange={handleDefenseChange}
+                    placeholder='Defense'
+                    type='number'
+                    min="0"
+                    required={true}
+                />
+                <InputField 
+                    label={'Speed:'}
+                    value={speed} 
+                    handleChange={handleSpeedChange}
+                    placeholder='Speed'
+                    type='number'
+                    min="0"
+                    required={true}
+                />
+                <button type='submit'>Save</button>
+            </form>
         </>
-        
     )
 }
 
